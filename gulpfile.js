b98e66d96377352402db393ff86eb12af25a3b39
Ben Foxall <ben@whiteoctober.co.uk>
2015-05-07 20:29:07 +0100

    all this for a less file

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minifyCss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var connect = require('gulp-connect');
+
+gulp.task('default', ['less'], function() {
+
+  gulp.watch(['style.less'], ['less']);
+  gulp.watch(['*.html'], ['html']);
+
+  connect.server({
+    root: '.',
+    livereload: true
+  });
+});
+
+
+gulp.task('less', function () {
+  return gulp.src('style.less')
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(minifyCss())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('.'))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+  return gulp.src('*.html')
+    // nothing
+    .pipe(connect.reload());
+});
